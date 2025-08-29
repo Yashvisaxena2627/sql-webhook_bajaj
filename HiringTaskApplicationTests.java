f65edccbcd58a5cea0b05ee3134b9d7dd6c1a1ce
Yashvisaxena2627 <157515673+Yashvisaxena2627@users.noreply.github.com>
2025-08-29 11:42:28 +0530

    Add files via upload

---
+++ HiringTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthrx.hiringtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiringTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test verifies that the Spring application context loads successfully
+    }
+}
